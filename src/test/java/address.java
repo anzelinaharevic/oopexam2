e8fcc7369cb70df1007a712c0e6025be8b28d66a
ANZELINAHAREVICA-STU <g00414991@atu.ie>
2025-04-08 10:18:34 +0100

    committing address changes

--- src/test/java/address.java
+++ src/test/java/address.java
@@ -9,7 +9,11 @@ public class address {
     @Test
     void ScannertestIsOpen(){
         assertNotEquals(9,12);
+        address[]{}),"address is too  short");
+    @Test
         assertNotEquals(41,12);
+        address[]{}),"address is too long");
+    @Test
         assertArrayEquals (
                 address[]{1,2,3,4,5,6,7,8,9,10,11,12}),"address testing done");
     }
