d377b4ae296d9e4abaa41e5acb3f17d7d1dbd6b5
ANZELINAHAREVICA-STU <g00414991@atu.ie>
2025-04-08 10:18:43 +0100

    committing customerID changes

--- src/test/java/customerID.java
+++ src/test/java/customerID.java
@@ -7,9 +7,13 @@ public class customerID {
         customerID = new customerID ("testing customerID");
     }
     @Test
-    void ScannertestIsOpen(){
-        assertNotEquals(7,8);
+         void ScannertestIsOpen(){
+         assertNotEquals(7,8);
+        customerID[]{1,2,3,4,5,6,7,8}),"customer id too short");
+        @Test
         assertNotEquals(13,8);
+        customerID[]{1,2,3,4,5,6,7,8}),"customer id is too long");
+        @Test
         assertArrayEquals (
                 customerID[]{1,2,3,4,5,6,7,8}),"customer id testing done");
     }
